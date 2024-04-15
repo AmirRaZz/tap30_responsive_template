028e44a6f5e7f1401cbebf844c1b4bfda928fdc9
RaZz <amirrahmati471@gmail.com>
2024-04-15 20:31:14 +0330

    develop travel and some extra sections and final optimizations

--- js/main.js
+++ js/main.js
@@ -23,3 +23,12 @@ tabs.forEach((tab) => {
         targeTabContent.classList.add("active");
     });
 });
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordion = document.querySelector(".accordion");
+accordionHeaders.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        console.log(e.target.parentElement);
+        e.target.parentElement.classList.toggle("accordion__expanded");
+    });
+});
